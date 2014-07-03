8361b2b47ade9371f89e5b2fc369a370288c6bd6
sailrancho <suqian@baidu.com>
2014-07-03 19:26:40 +0800

    can using start  procA to start one proc only

---
+++ processFrameWork/proc/ProcTest.php
@@ -0,0 +1,9 @@
+<?php
+class ProcTest extends ProcBasic
+{
+	public function run()
+	{
+		echo "std out outout.....\n";
+	}
+
+}
--- processFrameWork/server.php
+++ processFrameWork/server.php
@@ -6,10 +6,13 @@ if (isset($argv[1])) {
 }
 
 if (isset($argv[2])) {
-	echo $argv[2]; exit;
+	$start_procs = $argv[2];
+} else {
+	echo "please input the proc name your want to start|stop, using a or a-b , or a+b \n";
+	exit;
 }
 
-$server = new MainProc(require(CONF.DIRECTORY_SEPARATOR.'config.php'));
+$server = new MainProc(require(CONF.DIRECTORY_SEPARATOR.'config.php'), $start_procs);
 switch($option) {
 	case 'start' :
 		$server->start();
